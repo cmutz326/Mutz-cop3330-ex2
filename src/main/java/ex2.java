a922b82f4eb97b2aef72821fe25676d77c8a1e81
Christina Mutz <christinamutz@knights.ucf.edu>
2021-09-13 20:54:05 -0600

    Initial push

---
+++ src/main/java/ex2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class ex2 {
+
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+        int count = 0;                                               //make sure counting starts at 0
+     System.out.println("What is the input string?");                //ask user to say a word or phrase
+    var phrase = sc.nextLine();                               //save the phrase the user input
+
+   for (int i= 0; i < phrase.length(); i++)                           //tell system to count strings length
+        {
+            if(phrase.charAt(i) != ' ')                              //for each character, the count will go up
+                count++;
+        }
+
+
+
+
+    System.out.println(phrase + " has " + count + " characters.");   //tell the user how many characters the input contained
+
+    }
+
+}
